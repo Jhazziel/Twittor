9777520383654994de8a899967719deb11d89f73
Jhaziel Sarzuri <jhazielsr@gmail.com>
2019-02-18 14:15:06 -0400

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE      = 'static-v3';
+const STATIC_CACHE      = 'static-v1';
 const DYNAMIC_CACHE     = 'dynamic-v1';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
@@ -50,6 +50,10 @@ self.addEventListener('activate', e => {
         .then( keys => {
             keys.forEach( key => {
                 if ( key !== STATIC_CACHE && key.includes('static')) {
+                    return caches.delete(key);
+                }
+
+                if ( key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                     return caches.delete(key);
                 }
             });
