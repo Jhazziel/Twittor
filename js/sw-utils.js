e8f7bb155f4619e85f2a258286f1b761cea6d823
Jhaziel Sarzuri <jhazielsr@gmail.com>
2019-02-18 12:02:37 -0400

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,24 @@
+
+function actualizaCacheDinamico( dynamicCache, req, resp) {
+
+    if ( resp.ok ) {
+        return caches.open( dynamicCache )
+         .then( cache => {
+             cache.put( req, resp.clone() );
+             return resp.clone()
+         })
+    } else {
+        return resp;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
